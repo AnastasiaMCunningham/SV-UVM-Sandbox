2366516548b650e74ffb6896e47e32117ed9eeef
AnastasiaMCunningham <99664411+AnastasiaMCunningham@users.noreply.github.com>
2022-02-20 21:19:05 -0500

    cleaned up comments, changed arr[M:N] to select different range for each point in example

--- Graphing/dataPoint2D.sv
+++ Graphing/dataPoint2D.sv
@@ -32,29 +32,20 @@ module dataPoint2D;
     Point #(.dimension(1)) p1 = new;
     Point #(.dimension(2)) p2 = new;
     Point #(.dimension(3)) p3 = new;
-    //Point #(0) p0 = new(NULL);
-   // Point #(1) pBad1 = new(pos[0:5]); //will take in an array of pos that's too big
+    
+    //-- error checking --//
+    //Point #(0) p0 = new(); //will attempt to make an empty array, no coordinates
+    //Point #(1) pBad1 = new(pos[0:5]); //will take in an array of pos that's too big
     //Point #(2) pBad2 = new(pos[0]); //will take in an array of pos that's too small
     
     initial begin
     p1.populatePositions(arr[0:0]);
-    p2.populatePositions(arr[0:1]);
-    p3.populatePositions(arr[0:2]);
-    
-//    p1.setPos(pos[0]);
-//    p2.setPos(pos[0]);
-//    p3.setPos(pos[0]);
-   // p0.setPos({});
-   // pBad1.setPos(pos[0:2]);
-   // pBad2.setPos(pos);
+    p2.populatePositions(arr[1:2]);
+    p3.populatePositions(arr[3:5]);
     
     p1.printData();
     p2.printData();
     p3.printData();
-//    p0.printData();
-   // pBad1.printData();
-   // pBad2.printData();
-    
     end  
   
 endmodule
