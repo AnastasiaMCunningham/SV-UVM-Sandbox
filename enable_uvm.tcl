357a3bd8bfe4cc1ca0831c1a2a982b67934cbce6
AnastasiaMCunningham <99664411+AnastasiaMCunningham@users.noreply.github.com>
2022-03-01 13:51:09 -0500

    added comment describing how to run

--- enable_uvm.tcl
+++ enable_uvm.tcl
@@ -1,2 +1,4 @@
+#to run in tcl command line: source {path to file}/enable_uvm.tcl
+
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
