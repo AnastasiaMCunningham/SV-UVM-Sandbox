e403d5c263ab772be4c74e6a77a1b20397816366
AnastasiaMCunningham <99664411+AnastasiaMCunningham@users.noreply.github.com>
2022-03-01 13:49:24 -0500

    Fixed typo
    correction from previous commit: to run via tcl console in project, use:
    source {path}/enable_uvm.tcl

--- enable_uvm.tcl
+++ enable_uvm.tcl
@@ -1,2 +1,2 @@
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
-set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]_
+set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
